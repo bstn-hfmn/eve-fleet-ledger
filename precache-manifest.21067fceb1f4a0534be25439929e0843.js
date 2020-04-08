dc5c1cfb1dd115b05886d2cd57cdc2b933ffb64f
Bastian Hoffmann <bastian_hoffmann1999@gmx.de>
2020-04-08 15:04:29 +0200

    Updates

---
+++ precache-manifest.21067fceb1f4a0534be25439929e0843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c23ccc405edd13d30616b5d8c2db2e",
+    "url": "/eve-fleet-ledger/index.html"
+  },
+  {
+    "revision": "a2300d3d1ca7ff2deea8",
+    "url": "/eve-fleet-ledger/static/css/main.fc62393a.chunk.css"
+  },
+  {
+    "revision": "64775144bf47f84716fc",
+    "url": "/eve-fleet-ledger/static/js/2.069082c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eve-fleet-ledger/static/js/2.069082c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2300d3d1ca7ff2deea8",
+    "url": "/eve-fleet-ledger/static/js/main.ff71aeb5.chunk.js"
+  },
+  {
+    "revision": "37cbdad620a8f66384bf",
+    "url": "/eve-fleet-ledger/static/js/runtime-main.581e8fe8.js"
+  }
+]);
